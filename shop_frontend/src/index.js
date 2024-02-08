38c4a54ba6714d83bab72e40a736f793babff986
Chamika Dilshan <chamikadilshan1123@gmail.com>
2024-02-08 10:17:20 +0530

    index dila

--- shop_frontend/src/index.js
+++ shop_frontend/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AdminDisplay from './AdminDisplay';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/AdminDisplay' element={<AdminDisplay />} />
+
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
