da038ce1fd7fe8f933cb2b9367ddb1939a076719
anujauthsara <anuja.uthsara.2002@gmail.com>
2024-04-04 13:18:27 +0530

    anuja

--- shop_frontend/src/ShoppingCart.js
+++ shop_frontend/src/ShoppingCart.js
@@ -48,12 +48,15 @@ const ShoppingCart = () => {
   useEffect(() => {
     // Calculate total when carts or carts2 change
     let calculatedTotal = 0;
-    carts2.forEach(cart => {
-      const filteredCart = carts.find(c => c.id === cart.id);
-      if (filteredCart) {
-        calculatedTotal += filteredCart.price * cart.quantity;
-      }
-    });
+    carts2
+      .filter(cart => cart.email === userEmail)
+      .forEach(cart => {
+        const filteredCart = carts.find(c => c.id === cart.id);
+        if (filteredCart) {
+          calculatedTotal += filteredCart.price * cart.quantity;
+        }
+      });
+    
     setTotal(calculatedTotal);
   }, [carts, carts2]);
 
@@ -102,34 +105,37 @@ const ShoppingCart = () => {
       </div>
       <div className='shopping-cart'>
         <h2 className='cart-title'>Shopping Cart</h2>
-        {carts2.map((cart) => {
-          const imageUrlObj = imageUrls.find(urlObj => urlObj.id === cart.id);
-          const imageUrl = imageUrlObj ? imageUrlObj.url : null;
-          const filteredCart = carts.find(c => c.id === cart.id);
+        {carts2
+  .filter(cart => cart.email === userEmail)
+  .map((cart) => {
+    const imageUrlObj = imageUrls.find(urlObj => urlObj.id === cart.id);
+    const imageUrl = imageUrlObj ? imageUrlObj.url : null;
+    const filteredCart = carts.find(c => c.id === cart.id);
 
-          return (
-            <div className="cart-item" key={cart.id}>
-              <div className="item-photo">
-                {imageUrl ? (
-                  <img src={imageUrl} alt={`Photo-${cart.id}`} />
-                ) : (
-                  <img src="https://t4.ftcdn.net/jpg/05/07/58/41/360_F_507584110_KNIfe7d3hUAEpraq10J7MCPmtny8EH7A.jpg" alt="Placeholder" />
-                )}
-              </div>
-              <div className="item-details">
-                <h3 className="item-name">ID: {cart.id}</h3>
-                <h3 className="item-name">Name: {filteredCart ? filteredCart.name : 'Name not available'}</h3>
-                <p className="item-price">LKR. {filteredCart ? filteredCart.price * cart.quantity : 'Price not available'}/=</p>
-              </div>
-              <div className="item-actions">
-                <button className="action-button">-</button>
-                <span className="item-quantity">{cart.quantity}</span>
-                <button className="action-button">+</button>
-                <button className="action-button delete-button" onClick={() => {deleteCart(cart.id); handleButtonClick();}}>Delete</button>
-              </div>
-            </div>   
-          );
-        })}
+    return (
+      <div className="cart-item" key={cart.id}>
+        <div className="item-photo">
+          {imageUrl ? (
+            <img src={imageUrl} alt={`Photo-${cart.id}`} />
+          ) : (
+            <img src="https://t4.ftcdn.net/jpg/05/07/58/41/360_F_507584110_KNIfe7d3hUAEpraq10J7MCPmtny8EH7A.jpg" alt="Placeholder" />
+          )}
+        </div>
+        <div className="item-details">
+          <h3 className="item-name">ID: {cart.id}</h3>
+          <h3 className="item-name">Name: {filteredCart ? filteredCart.name : 'Name not available'}</h3>
+          <p className="item-price">LKR. {filteredCart ? filteredCart.price * cart.quantity : 'Price not available'}/=</p>
+        </div>
+        <div className="item-actions">
+          <button className="action-button">-</button>
+          <span className="item-quantity">{cart.quantity}</span>
+          <button className="action-button">+</button>
+          <button className="action-button delete-button" onClick={() => {deleteCart(cart.id); handleButtonClick();}}>Delete</button>
+        </div>
+      </div>   
+    );
+})}
+
       </div>
       Total: {total}
     </div>
